67336bb39908d4a1343cb7f8396de466b965afcc
Ryan Dawes <dev@dawes.cc>
2023-04-27 09:58:41 -0400

    removed auth and removed boilerplate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1> Hello </h1>
+      <h1>Hello</h1>
     </main>
   )
 }
