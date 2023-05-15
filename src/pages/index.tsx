7d846ab66834430f1a25ca1040672fd901c4be12
Ryan Dawes <dev@dawes.cc>
2023-05-15 09:37:22 -0400

    m1 setup commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello</h1>
+      <h1>Hello again</h1>
     </main>
   )
 }
